35bf6325f31649d5c8a8be2d9df577e2d33dc598
Ryan Ellis <ryanpatrickellis@gmail.com>
2019-01-05 13:05:35 -0500

    most common word

--- leetcode/819 - Most Common Word.java
+++ leetcode/819 - Most Common Word.java
@@ -1,31 +1,34 @@
+// Idea: Pretty straight forward hashmap/set problem. The hardest part is 
+//       using the correct regex to filter the punctuation from the paragraph.
+
+// Run Time: O(n^2) b/c we have to iterate through all the words in the paragraph and
+//           then we have to iterate through all the values in the map. Feel like this
+//           could definetly be improved without having to iterate through the map values
+//           to find the max.
+
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        HashSet<String> bannedWords = new HashSet<>();
         HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<String>(Arrays.asList(banned));
         
-        for(String str : banned){
-            bannedWords.add(str);
-        }
+        String[] words = paragraph.replaceAll("\\W+" , " ").toLowerCase().split("\\s");
         
-        String[] words = paragraph.toLowerCase().replaceAll(", .", "|").split("|");
-        
-        for(String word : words){
-            if(bannedWords.contains(word)){
-                if(map.containsKey(word)){
-                    map.put(word, map.get(word) + 1);
+        for(String str : words){
+            if(!set.contains(str)){
+                if(map.containsKey(str)){
+                    map.put(str, map.get(str) + 1);
                 } else{
-                    map.put(word, 1);
+                    map.put(str, 1);
                 }
             }
         }
         
         int largest = 0;
         String answer = "";
-        
         for(String key : map.keySet()){
             if(map.get(key) > largest){
-                largest = map.get(key);
                 answer = key;
+                largest = map.get(key);
             }
         }
         
